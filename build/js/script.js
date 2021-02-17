362ea9e878956a767329e8f3c580877ff19c5d27
Алексей Кухаренко <druggist5059@gmail.com>
2021-02-17 11:48:10 +0300

    Обновляет build

--- build/js/script.js
+++ build/js/script.js
@@ -107,3 +107,58 @@ contactsToggle.addEventListener('click', function () {
     contacts.classList.add('contacts-footer--close');
   }
 });
+
+var phoneInput = document.querySelectorAll('input[type="tel"]');
+
+phoneInput.forEach(function (input) {
+  input.addEventListener('keypress', function (evt) {
+    if (evt.keyCode < 47 || evt.keyCode > 57) {
+      evt.preventDefault();
+    }
+  });
+
+  input.addEventListener('focus', function () {
+    if (input.value.length === 0) {
+      input.value = '+7(';
+      input.selectionStart = input.value.length;
+    }
+  });
+
+  input.addEventListener('click', function (evt) {
+    if (input.selectionStart < 3) {
+      input.selectionStart = input.value.length;
+    }
+    if (evt.key === 'Backspace' && input.value.length <= 3) {
+      evt.preventDefault();
+    }
+  });
+
+  input.addEventListener('blur', function () {
+    if (input.value === '+7(') {
+      input.value = '';
+    }
+  });
+
+  input.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Backspace' && input.value.length <= 3) {
+      evt.preventDefault();
+    }
+    var currentLength = input.value.length;
+
+    if (evt.key === 'Backspace') {
+      currentLength--;
+
+      return;
+    }
+
+    if (currentLength === 6) {
+      input.value = input.value + ') ';
+    }
+    if (currentLength === 11) {
+      input.value = input.value + ' ';
+    }
+    if (currentLength === 14) {
+      input.value = input.value + ' ';
+    }
+  });
+});
